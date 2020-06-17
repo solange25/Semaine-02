1918b241a0385b9ffcde42677db9a2cbf7dfdd70
solange25 <mariesolange@siriusschool.be>
2020-06-17 10:00:47 +0200

    update javascript

---
+++ Exercices/exercices-javascript/les-variables/script.js
@@ -0,0 +1,20 @@
+const helloWorld = 'Hello World';
+console.log(helloWorld);
+const myNew = "Bonjour je m'appelle";
+let prénom = 'Solange';
+console.log(myNew + ' ' + prénom);
+const myMe = "j'ai";
+let myAge = '29 ans';
+console.log(myNew + ' ' + prénom + ' ' + myMe + ' ' + myAge);
+let siriusTeamArray;
+let myGoupe = [
+  'solange',
+  'nana',
+  'claudine',
+  'nadia',
+  'papi',
+  'kudu',
+  'tania',
+  'mani',
+];
+console.log(myGoupe[6]);
